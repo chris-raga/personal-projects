2cf82f5bc8b047d19c0e605c2ffe604655895f4b
Christian Alan Ragadio <christian.ragadio2@gmail.com>
2023-02-19 18:40:00 -0800

    new stuff tested and added, learned getline, stoi, try & catch, const invalid_argument&

---
+++ charsAndInputs.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+char f = 'F';
+char u = 'U';
+string firstName;
+string lastName;
+
+cout << "How are you? \nWhat is your first and last name?\n";
+
+cin >> firstName >> lastName;
+
+cout << f << " "<< u << " " << firstName << " " << lastName;
+
+
+return 0;
+}
